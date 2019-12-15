314f7d63afbc7aabe9b433c7829d8e128c1b4ea3
Alex Yuan <alex@sydneydigitalgroup.com.au>
2019-12-15 16:28:38 +1100

    149

--- 149.Max Points on a Line.py
+++ 149.Max Points on a Line.py
@@ -1,5 +1,12 @@
+#Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        for i in points:
-            for j in points:
-                
+        x=0
+        y=1
+        length = len(points)
+        if length < 3: 
+            return length
+        
+        for i in range length:
+            for j in range (i,length)：
+
