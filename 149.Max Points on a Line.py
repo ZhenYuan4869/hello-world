7fe0a2bf5f7e757b222f612eea5d3890dfb1c4a1
Zhen Yuan <jhen.yua@gmail.com>
2019-12-22 06:29:13 +1100

    149

--- 149.Max Points on a Line.py
+++ 149.Max Points on a Line.py
@@ -1,4 +1,5 @@
 #Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+#python 3
 class Solution:
     def gcd(self, x, y):
         return x if y == 0 else self.gcd(y, x % y)
@@ -12,11 +13,23 @@ class Solution:
             return length
         
         for i in range (length):
-            lines = collections.defaultdict(int) // 定义了一个类，相当于可以将key由自己定义的一个dict
-            deplicated = 1  //重复的点
+            lines = collections.defaultdict(int) # 定义了一个类，相当于可以将key由自己定义的一个dict
+            duplicated = 1  #重复的点
             for j in range (i+1,length):
-                if (points[i][x] == points[j][x] & points[i][y] == points[j][y]): //只要考虑后面的点就好了，因为不会超过前面的
-                    deplicated += 1
+                if (points[i][0] == points[j][0] and points[i][1] == points[j][1]): #只要考虑后面的点就好了，因为不会超过前面的
+                    duplicated += 1
+                    continue #加一之后不用进行其他的操作了
+                dx = points[i][0] - points[j][0]
+                dy = points[i][1] - points[j][1]
+                delta = self.gcd(dx, dy) 
+                if delta == 0:
+                    lines[1] += 1
+                else:
+                    lines[(dx / delta, dy / delta)] += 1
+            res = max(res, (max(lines.values()) if lines else 0) + duplicated) # 最大的res和最新的作比较
+        return res
+
+                
                 
 
                 
