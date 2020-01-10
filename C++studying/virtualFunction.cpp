306c246b293c5d22313852a9d024673be51d670d
Zhen Yuan <jhen.yua@gmail.com>
2020-01-11 04:59:58 +1100

    C++

--- C++studying/virtualFunction.cpp
+++ C++studying/virtualFunction.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+//test envioroment
 // using namespace std;
 // int main()
 // {
@@ -7,3 +8,27 @@
 // }
 #include <string> 
 class parent
+{
+    public:
+        void Test()
+        {
+            printf("parent function part\n");
+        }
+};
+
+class Child: public parent
+{
+    public:
+        void Test()
+        {
+            parent::Test();
+            printf("child function part\n"); //原函数增加一行运行,相当于重载test函数
+        }
+};
+ int main()
+ {
+     Child c;
+     c.Test();
+
+     return 0;
+ }
