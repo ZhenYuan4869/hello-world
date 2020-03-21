06168269bb33ca9bdb48607eb5536e219994fe91
Alex Yuan <1244979103@qq.com>
2020-03-21 20:03:32 +0800

    question

--- five_monkey_share_peach.cpp
+++ five_monkey_share_peach.cpp
@@ -12,4 +12,23 @@ int main()
         cout<<total_num<<" "<<old_num<<endl;  
     }  
     return 0;  
-}  
+}
+
+int solution2()
+{
+    int n, i;
+    int sum, l, t;
+    while (scanf("%d", &n)==1 && n)
+    {
+        l = 1;
+        for (i=1; i<n; i++)
+            l *= 5;
+        sum = 1+5*(l-1);
+        t = sum;
+        for (i=1; i<=n-1; i++)
+            t = (t-1)/5*4;
+        printf("%d %d\n", sum, t+n);
+    }
+    printf("%d %d\n", sum, t+n);
+    return 0;
+}
