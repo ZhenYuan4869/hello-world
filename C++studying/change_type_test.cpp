88e0a8477201439dc0248b7b7fb21f72a916d2f7
Alex Yuan <1244979103@qq.com>
2020-03-24 04:12:46 +0800

    test

--- C++studying/change_type_test.cpp
+++ C++studying/change_type_test.cpp
@@ -8,7 +8,9 @@ int main()
     char b = '5';
     string s = "45";
     a = b - '0'+'0';//这里a为asc的相差
-    
     cout<<a<<endl;
+    char c = '6';
+    c = c+'0';
+    cout<<c<<endl;
     return 0;
 }
