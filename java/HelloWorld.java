62c275c5c41afe8914f31bdd294da0a56ccfb51a
Alex Yuan <1244979103@qq.com>
2020-06-19 05:34:50 +0800

    test

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+public class HelloWorld{ //class名字和文件名字要一样
+    public static void main(String[] args){ //main方法，程序入口
+        System.out.println("Hello World!");//自带换行
     }
 }
