f2be266e6774dfbd23cd833ef615835086005499
Alex Yuan <1244979103@qq.com>
2020-06-19 05:43:06 +0800

    java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{ //class名字和文件名字要一样
     public static void main(String[] args){ //main方法，程序入口
         System.out.println("Hello World!");//自带换行
+        System.out.println(5/6.0);
+        int x = 3;
+        int y = 2*x;    //函数的定义和计算
+        System.out.println(y);
     }
 }
