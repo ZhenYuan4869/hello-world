c72095cebd54f9ab530cf65f054e54b5d8c15ad3
Alex Yuan <1244979103@qq.com>
2020-07-09 01:51:37 +0800

    mutithread

--- java/mutithread/src/multithread1/inputNumbersOfThreads.java
+++ java/mutithread/src/multithread1/inputNumbersOfThreads.java
@@ -2,7 +2,7 @@
  * @author Zhen.Yuan
  * @date 2020-07-09
  */
-//方法一采用普通的建立线程然后重写runnable的方法输出
+//方法一采用普通的建立线程然后重写runnable的方法输出，同时用递归方法控制输出
 package multithread1;
 
 import java.util.Scanner;
@@ -13,15 +13,13 @@ public class inputNumbersOfThreads{
         System.out.println("请输入需要建立的线程数：");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        for(int i=0;i<number;i++){
-            new Thread(new myThread(i+1)).start(); //以i为编号创建线程并且开始运行
+        new Thread(new myThread(number)).start(); //以i为编号创建线程并且开始运行
             //start()方法开启一个线程，此时并没有运行，一旦得到CPU时间片，就自动开始执行run()方法。
-        }
         System.out.println("等待程序运行中...");
     }
 }
 
-class myThread implements Runnable {
+class myThread extends Thread implements Runnable {
     private int number;   //线程编号
 
     myThread(int i) {
@@ -30,12 +28,22 @@ class myThread implements Runnable {
 
     @Override
     public void run() {
+        if(this.number>1){
+            myThread t = new myThread(number-1);
+            try {
+                t.start();//递归方法
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         if (this.number % 3 == 0) {
-            System.out.println("当前线程为：" + this.number + "，输出为：" + (this.number % 3 + 3) + "。");
+            System.out.println("当前线程号为：" + this.number + "，输出为：" + (this.number % 3 + 3) + "。");
         } else {
-            System.out.println("当前线程为：" + this.number + "，输出为：" + (this.number % 3) + "。");
+            System.out.println("当前线程号为：" + this.number + "，输出为：" + (this.number % 3) + "。");
         }
     }
+
 }
 
 
