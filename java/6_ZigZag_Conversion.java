a731a5122616254bbef6a985ad9ab678dcb68c12
Alex Yuan <1244979103@qq.com>
2020-07-17 00:44:39 +0800

    leetcode

--- java/6_ZigZag_Conversion.java
+++ java/6_ZigZag_Conversion.java
@@ -1,5 +1,11 @@
 class Solution {
     public String convert(String s, int numRows) {
+        //先考虑如果一行就直接输出
+        if(numRows==1){
+            return s;
+        }
+        
+        String result[3][s.length()];
         for(int i = 0;i<s.length();i++){
             
         }
