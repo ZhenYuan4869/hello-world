5979d28b03cb89aae885a9ec41575772d60202cb
Alex Yuan <1244979103@qq.com>
2020-07-17 00:49:22 +0800

    leetcode

--- java/6_ZigZag_Conversion.java
+++ java/6_ZigZag_Conversion.java
@@ -1,12 +1,13 @@
+import java.util.List;
+
 class Solution {
     public String convert(String s, int numRows) {
         //先考虑如果一行就直接输出
         if(numRows==1){
             return s;
         }
+//从左到右迭代 ss，将每个字符添加到合适的行。可以使用当前行和当前方向这两个变量对合适的行进行跟踪。
+// 只有当我们向上移动到最上面的行或向下移动到最下面的行时，当前方向才会发生改变。
+        List<String> 
         
-        String result[3][s.length()];
-        for(int i = 0;i<s.length();i++){
-            
-        }
     }
