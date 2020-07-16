ea4428596991dc6fa5707fbf56d529f62359bc8a
Alex Yuan <1244979103@qq.com>
2020-07-17 01:56:35 +0800

    leetcode

--- java/6_ZigZag_Conversion.java
+++ java/6_ZigZag_Conversion.java
@@ -1,3 +1,15 @@
+// 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+// 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+// L   C   I   R
+// E T O E S I I G
+// E   D   H   N
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+// 请你实现这个将字符串进行指定行数变换的函数：
+// string convert(string s, int numRows);
+
+
+//其实不需要考虑N一样排列，只要考虑放在第几行，结果是一样的，这算是一个解题点
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
@@ -7,7 +19,9 @@ class Solution {
             return s;
         }
 //从左到右迭代 ss，将每个字符添加到合适的行。可以使用当前行和当前方向这两个变量对合适的行进行跟踪。
-// 只有当我们向上移动到最上面的行或向下移动到最下面的行时，当前方向才会发生改变。
-        List<String> 
-        
+//只有当我们向上移动到最上面的行或向下移动到最下面的行时，当前方向才会发生改变。
+//这里我考虑用numRows个String来记录数据
+        for (int i = 0; i < Math.min(output, s.length()); i++){
+            
+        }
     }
