bdab7494b343c354d2d82d10510bbfbc8da5abf2
Alex Yuan <1244979103@qq.com>
2020-09-03 23:47:54 +0800

    package

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,4 +1,6 @@
-public class HelloWorld{ //class名字和文件名字要一样
+package java;
+
+public class HelloWorld { // class名字和文件名字要一样
     public static void main(String[] args){ //main方法，程序入口
         System.out.println("Hello World!");//自带换行mysql -u root -p
         System.out.println(5/6.0);
